a158c2a61ca25892330a5ef92f916b642c48344d
Eden Turgeman <edmx0.et@gmail.com>
2022-09-18 11:07:36 +0300

    Export esm only :)

--- rollup.config.js
+++ rollup.config.js
@@ -1,27 +1,30 @@
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
-import typescript from 'rollup-plugin-typescript2';
-const packageJson = require('./package.json');
+import postcss from 'rollup-plugin-postcss';
+import typescript from "@rollup/plugin-typescript";
+import packageJson from './package.json'
 
 export default {
   input: 'src/index.ts',
   output: [
     {
       file: packageJson.main,
-      format: 'cjs',
-      sourcemap: true,
+      format: 'esm',
     },
     {
-      file: packageJson.module,
       format: 'esm',
-      sourcemap: true,
+      dir: 'lib',
+      preserveModules: true,
     },
   ],
   plugins: [
     peerDepsExternal(),
     resolve(),
     commonjs(),
-    typescript({ useTsconfigDeclarationDir: true, exclude: ['**/__tests__', '**/*.(test|spec).(ts|tsx)'] }),
-  ],
+    postcss({
+      extract: false,
+    }),
+    typescript()
+],
 };
