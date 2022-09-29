293e8af4f3c0dd32b3b4c52fab81e8d6a6d5ff9b
Eden Turgeman <edmx0.et@gmail.com>
2022-09-29 10:17:01 +0300

    remove commonJs

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,5 @@
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
 import postcss from 'rollup-plugin-postcss';
 import typescript from "@rollup/plugin-typescript";
 import packageJson from './package.json'
@@ -21,7 +20,6 @@ export default {
   plugins: [
     peerDepsExternal(),
     resolve(),
-    commonjs(),
     postcss({
       extract: false,
     }),
